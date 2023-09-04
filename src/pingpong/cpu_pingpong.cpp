2e6799e2c12d99eb2d4a5cfed24f71f94452eedb
Zhang690683220 <zhangbo_china@live.cn>
2023-09-03 18:13:00 -0600

    add node_aware communication

--- src/pingpong/cpu_pingpong.cpp
+++ src/pingpong/cpu_pingpong.cpp
@@ -8,13 +8,28 @@ int main(int argc, char** argv) {
     int tag1 = 10;
 	int tag2 = 20;
     
-    int nprocs, rank, partner_rank;
+    int nprocs, rank, local_rank, partner_rank;
 
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    std::cout<< "Start MPI Allgather between 2 processes." << std::endl;
+    MPI_Comm shmcomm;
+    MPI_Comm_split_type(MPI_COMM_WORLD, MPI_COMM_TYPE_SHARED, 0,
+                        MPI_INFO_NULL, &shmcomm);
+    MPI_Comm_rank(shmcomm, &local_rank);
+
+    int *loc_ranks = (int*) malloc(nprocs*sizeof(int));
+    MPI_Allgather(&local_rank, 1, MPI_INT, loc_ranks, 1, MPI_INT, MPI_COMM_WORLD);
+    for(int i=0; i<nprocs; i++) {
+        if((i != rank) && (loc_ranks[i] == loc_ranks[rank])) {
+            partner_rank = i;
+            break;
+        }
+    }
+    free(loc_ranks);
+
+    std::cout<< "Start MPI Allgather between rank " << rank << " <--> " << partner_rank << std::endl;
 
     double *host_buf;
 
@@ -25,12 +40,12 @@ int main(int argc, char** argv) {
 
     while(1) {
         if(rank == 0){
-            MPI_Send(host_buf, elem_num, MPI_DOUBLE, 1, tag1, MPI_COMM_WORLD);
-            MPI_Recv(host_buf, elem_num, MPI_DOUBLE, 1, tag2, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            MPI_Send(host_buf, elem_num, MPI_DOUBLE, partner_rank, tag1, MPI_COMM_WORLD);
+            MPI_Recv(host_buf, elem_num, MPI_DOUBLE, partner_rank, tag2, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         }
         else if(rank == 1){
-            MPI_Recv(host_buf, elem_num, MPI_DOUBLE, 0, tag1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            MPI_Send(host_buf, elem_num, MPI_DOUBLE, 0, tag2, MPI_COMM_WORLD);
+            MPI_Recv(host_buf, elem_num, MPI_DOUBLE, partner_rank, tag1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            MPI_Send(host_buf, elem_num, MPI_DOUBLE, partner_rank, tag2, MPI_COMM_WORLD);
         }
     }
 
